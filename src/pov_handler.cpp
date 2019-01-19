63f8c603c28604c8a7d0f9fad0f3498e75aa4f89
Pavlo Boidachenko <boidachenkopavlo@gmail.com>
2019-01-19 14:31:39 +0100

    Minor fix to camera home position

--- src/pov_handler.cpp
+++ src/pov_handler.cpp
@@ -2,7 +2,7 @@
 * @Author: Pavlo Boidachenko
 * @Date:   2019-01-09 14:27:08
 * @Last Modified by:   Pavlo Boidachenko
-* @Last Modified time: 2019-01-19 14:10:52
+* @Last Modified time: 2019-01-19 14:29:24
 */
 
 #include "pov_handler.hpp"
@@ -15,17 +15,11 @@ using osgGA::GUIEventHandler;
 
 POVHandler::POVHandler(const ref_ptr<Viewer> & viewer, Cannon* cannon)
 : _viewer(viewer), _cannon(cannon){ 
-	_camera = _viewer->getCamera();
 
 	manip = new NodeTrackerManipulator();
-	manip->setHomePosition(Vec3d(0, 100, 40), Vec3d(0, -100, 0), Vec3d(0,0,1));
+	manip->setHomePosition(Vec3d(0, 80, 40), Vec3d(0, -200, 10), Vec3d(0,0,1));
 	manip->setTrackerMode(NodeTrackerManipulator::NODE_CENTER_AND_ROTATION);
 	manip->setTrackNode(cannon->get());
-
-	// ref_ptr<KeySwitchMatrixManipulator> keySwitch = new KeySwitchMatrixManipulator();
-	// keySwitch->addMatrixManipulator( '1', "Trackball", new TrackballManipulator );
-	// keySwitch->addMatrixManipulator( '2', "NodeTracker", manip.get() );
-	// _viewer->setCameraManipulator(keySwitch.get());
 	_viewer->setCameraManipulator(manip);
 }
 bool POVHandler::handle(const GUIEventAdapter & ea,
